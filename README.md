3cf07170665ac5d752d8e5b2831994668223f62c
fooldoc <949477774@qq.com>
2019-08-13 17:50:26 +0800

    gitfool

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # gitfool
-软连接
-sudo ln -s gitfool.sh /usr/local/bin/gitfool
 
-gitfool-(git傻瓜工具)
+## 软连接
+## sudo ln -s gitfool.sh /usr/local/bin/gitfool
 
-      update     获取最新内容到本地分支
-      reset      撤销提交记录到指定状态
-      checkout   检出一个分支或路径到工作区
-      log        显示提交日志
-      revertAll  恢复数据为最新版
-      revert     恢复某个文件的最新代码
+## gitfool-(git傻瓜工具)
+
+update   获取最新内容到本地分支 示例：gitfool update 
+reset  撤销提交记录到指定状态  示例：gitfool reset 
+checkout 检出一个分支或路径到工作区 示例：gitfool checkout 
+log 显示提交日志 示例：gitfool log 
+revertAll 恢复数据为远程最新版 示例：gitfool revertAll .  
+revert 恢复某个文件为远程最新版代码 示例：gitfool revert aa  
--- gitfool.sh
+++ gitfool.sh
@@ -184,12 +184,12 @@ case $action in
 		echo "Usage: $0 {update|reset|checkout|log|revert|revertAll}"
 		printf "$GREEN"
 		echo ""
-		printf "%5s %-10s %s\n" '' update 获取最新内容到本地分支
-		printf "%5s %-10s %s\n" '' reset  撤销提交记录到指定状态
-		printf "%5s %-10s %s\n" '' checkout 检出一个分支或路径到工作区
-		printf "%5s %-10s %s\n" '' log 显示提交日志
-		printf "%5s %-10s %s\n" '' revertAll 恢复数据为最新版
-		printf "%5s %-10s %s\n" '' revert 恢复某个文件的最新代码
+		printf "%5s %-10s %s\n" '' "update  获取最新内容到本地分支 示例：gitfool update"
+		printf "%5s %-10s %s\n" '' "reset  撤销提交记录到指定状态  示例：gitfool reset"
+		printf "%5s %-10s %s\n" '' "checkout 检出一个分支或路径到工作区 示例：gitfool checkout"
+		printf "%5s %-10s %s\n" '' "log 显示提交日志 示例：gitfool log"
+		printf "%5s %-10s %s\n" '' "revertAll 恢复数据为远程最新版 示例：gitfool revertAll . "
+		printf "%5s %-10s %s\n" '' "revert 恢复某个文件为远程最新版代码 示例：gitfool revert aa "
 		echo ""
 		exit 1;;
 	*)
